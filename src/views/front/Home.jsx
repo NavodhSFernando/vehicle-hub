747938aa552043c1bde117d8e4352b536237d33e
NavodhSFernando <fernandons.21@uom.lk>
2024-03-06 21:21:02 +0530

    fixed merge conflict

--- src/views/front/Home.jsx
+++ src/views/front/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import VehicleFleet from '../../components/front/VehicleFleet'
+import Image from '../../assets/images/index-image.png'
+import BookingStrip from '../../components/front/BookingStrip/BookingStrip'
 
 export default function Home() {
     return (
         <div>
             <div className="relative h-screen">
-                {/* <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${Image})` }}></div> */}
-                {/* <div className="absolute inset-0 flex justify-center items-center"> */}
-                <VehicleFleet />
+                <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${Image})` }}></div>
+                <div className="absolute inset-0 flex justify-center items-center">
+                    <BookingStrip />
+                </div>
             </div>
         </div>
     )
