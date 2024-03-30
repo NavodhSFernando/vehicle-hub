42ed717aa84f887fb6fdeda0278ee6f489536bb3
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-03-30 08:17:07 +0530

    billing details

---
+++ src/views/front/billingDetails/DataTable.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    getFilteredRowModel,
+    getSortedRowModel
+} from '@tanstack/react-table'
+import { Input } from '../../../components/ui/input'
+import { Label } from '../../../components/ui/label'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../../components/ui/table'
+
+export default function DataTable({ columns, data }) {
+    const [columnFilters, setColumnFilters] = React.useState([])
+    const [sorting, setSorting] = React.useState([])
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        state: {
+            sorting
+        }
+    })
+
+    return (
+        <div>
+            <div className="flex flex-row items-center my-8">
+                <div className="flex flex-col space-y-1 pt-2 w-full pb-4">
+                    <Label>Reservation ID</Label>
+                    <Input
+                        placeholder="Filter Reservation ID"
+                        value={table.getColumn('id')?.getFilterValue() ?? ''}
+                        onChange={(event) => table.getColumn('id')?.setFilterValue(event.target.value)}
+                        className="max-w-lg"
+                    />
+                </div>
+            </div>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow className="bg-slate-200" key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead className="py-1 px-5" key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(header.column.columnDef.header, header.getContext())}
+                                        </TableHead>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell className="py-5 px-5" key={cell.id}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+        </div>
+    )
+}
