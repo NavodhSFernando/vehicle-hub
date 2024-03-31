ed1b00ccc89d96ecd9d0a350e8f05617787786a0
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-03-31 15:35:10 +0530

    dashboard

--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import DashboardStatsGrid from './Dashboard/DashboardStatsGrid'
+import TransactionChart from './Dashboard/TransactionChart'
 
 export default function Dashboard() {
     return (
         <div className="flex flex-col gap-4 p-[10px]">
             <DashboardStatsGrid />
+
+            <div className="flex flex-row gap-4 w-full">
+                <TransactionChart />
+                {/* <BuyerProfilePieChart /> */}
+            </div>
         </div>
     )
 }
