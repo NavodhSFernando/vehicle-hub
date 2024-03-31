ba013f3ef9020e5488d360f0d2316fc9a646857b
Kaanchanaa <112231738+Kaanchanaa@users.noreply.github.com>
2024-03-31 12:11:04 +0530

    date and time picker component fix

--- src/components/front/BookingStrip/TimePicker.jsx
+++ src/components/front/BookingStrip/TimePicker.jsx
@@ -12,9 +12,6 @@ export default function TimePicker() {
         <div className="flex gap-2 items-center">
             <FaRegClock strokeWidth={1} fontSize={22} className="text-[#283280]" />
             <div className="flex flex-col w-fit">
-                <label htmlFor="Pick-Up-Date">
-                    <p className="text-[12px] opacity-80">Pick Up Time</p>
-                </label>
                 <input type="time" className="w-fit max-w-[120px] outline-none relative " placeholder="10:00:00" />
             </div>
         </div>
