1c6e915329f69232fa26d3d1967a7b212a19ed43
Sasanka Ravindu <61648299+SasankaRW@users.noreply.github.com>
2024-03-31 12:03:09 +0530

    home page fixed
    home page fixed

--- src/views/front/Home.jsx
+++ src/views/front/Home.jsx
@@ -4,13 +4,16 @@ import BookingStrip from '../../components/front/BookingStrip/BookingStrip'
 
 export default function Home() {
     return (
-        <div>
-            <div className="relative h-screen">
-                <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${Image})` }}></div>
-                <div className="absolute inset-0 flex justify-center items-center">
-                    <BookingStrip />
-                </div>
-            </div>
-        </div>
-    )
+       
+        
+               <div className="relative h-screen">
+                  <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${Image})` }}></div>
+                  <div className="absolute bottom-12 left-0 right-0 pb-32">
+                    <div className="flex justify-center items-end h-full">
+                        <BookingStrip />
+                    </div>
+                   </div>
+              </div> 
+        
+     )
 }
