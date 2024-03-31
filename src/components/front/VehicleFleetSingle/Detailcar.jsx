0685819eade24d903acd8ac769ed1c59da38cb7e
NavodhSFernando <fernandons.21@uom.lk>
2024-03-31 15:11:48 +0530

    added booking confirmation redirect

--- src/components/front/VehicleFleetSingle/Detailcar.jsx
+++ src/components/front/VehicleFleetSingle/Detailcar.jsx
@@ -2,19 +2,27 @@ import { React, useState } from 'react'
 import Datepicker from './Datepicker'
 import TimePicker from './TimePicker'
 import { Checkbox } from '../../ui/checkbox'
+import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs'
+import { Button } from '../../ui/button'
 
-export default function Detailcar() {
+export default function Detailcar({ vehicle, transmission, capacity, engine, mileage, fuel, year, colour, rate }) {
     const [rating, setRating] = useState(0)
 
     const handleStarClick = (value) => {
         setRating(value)
     }
+
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
     return (
-        <div className="max-w-[600px] w-full bg-white p-6 rounded-lg">
+        <div className="w-full bg-white p-6 rounded-lg">
             {/* Header */}
-            <article className="w-full flex justify-between pb-7">
+            <article className="w-full flex justify-between pb-7 p">
                 <div className="flex flex-col">
-                    <h1 className="text-2xl font-semibold text-gray-950 mb-1">Toyota Aqua</h1>
+                    <h1 className="text-2xl font-semibold text-gray-950 mb-1">{vehicle}</h1>
                     <div className="flex items-center">
                         {[...Array(5)].map((_, index) => (
                             <svg
@@ -32,48 +40,48 @@ export default function Detailcar() {
                         <p className="text-gray-500 text-xs">10+ Reviewer</p>
                     </div>
                 </div>
-                <button className="w-[35px] h-[38px] bg-black">
-                    <span className="w-[35px] h-[38px] bg-black"></span>
-                </button>
+                <div className="mt-1">
+                    {clicked ? (
+                        <BsBookmarkStarFill fontSize={24} onClick={handleClick} />
+                    ) : (
+                        <BsBookmarkStar fontSize={24} onClick={handleClick} />
+                    )}
+                </div>
             </article>
             {/* specification */}
             <div className="w-full pb-14">
                 <p className="text-sm text-slate-500 uppercase ">Specification</p>
-                <div className="p-2 grid grid-cols-2">
-                    <div className="flex flex-col">
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                <div className="p-2 flex gap-10">
+                    <div className="flex flex-col gap-1 w-1/2">
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-3/5">Transmission</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{transmission}</p>
                         </div>
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-3/5">Capacity</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{capacity}</p>
                         </div>
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-3/5">Engine</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{engine}</p>
                         </div>
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-3/5">Mileage</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{mileage}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-1">
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                    <div className="flex flex-col gap-1 w-1/2">
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-2/5">Fuel</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{fuel}</p>
                         </div>
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-2/5">Year</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{year}</p>
                         </div>
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
-                        </div>
-                        <div className="flex gap-5 w-full">
-                            <p className="text-lg text-slate-500">Capacity</p>
-                            <p className="text-lg text-slate-500 font-bold">4 Persons</p>
+                        <div className="flex w-full">
+                            <p className="text-lg text-slate-500 w-2/5">Colour</p>
+                            <p className="text-lg text-slate-500 font-bold w-2/5">{colour}</p>
                         </div>
                     </div>
                 </div>
@@ -97,9 +105,9 @@ export default function Detailcar() {
                 <p className="text-sm text-slate-500 uppercase ">Rates</p>
                 <div className="p-3">
                     <h1 className="text-3xl font-bold pb-2 w-full border-b border-black mb-4">
-                        Rs 10,000.00 / <span className="text-slate-500">day</span>
+                        {'Rs' + rate} / <span className="text-slate-500">day</span>
                     </h1>
-                    <div className="flex flex-col pb-10">
+                    <div className="flex flex-col pb-14">
                         <span className="flex gap-2 items-center mb-2">
                             <button className="w-[73px] flex justify-center rounded border border-black" disabled>
                                 <p className="text-sm">100 Km</p>
@@ -132,9 +140,9 @@ export default function Detailcar() {
                         </label>
                     </div>
 
-                    <button className="bg-blue-900 rounded w-full">
-                        <p className="text-sm text-white py-2">Request Vehicle</p>
-                    </button>
+                    <Button className="text-[#FBDAC6] bg-[#283280] hover:bg-[#283299] py-2.5 px-5 w-full rounded-lg text-sm">
+                        Request Vehicle
+                    </Button>
                 </div>
             </div>
         </div>
--- src/views/front/VehicleFleetSingle.jsx
+++ src/views/front/VehicleFleetSingle.jsx
@@ -1,16 +1,38 @@
 import RequestVehicle from '../../components/front/RequestVehicle'
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import Detailcar from '../../components/front/Navodshit/Detailcar'
+import Detailcar from '../../components/front/VehicleFleetSingle/Detailcar'
 
 export default function VehicleFleetSingle() {
     const { slug } = useParams()
 
+    const vehicle = {
+        name: 'Toyota Aqua',
+        transmission: 'Manual',
+        capacity: '6 Person',
+        engine: '1500cc',
+        mileage: '15km/l',
+        fuel: 'Petrol',
+        year: '2017',
+        colour: 'White',
+        rate: '15 000'
+    }
+
     return (
-        <div className="flex">
+        <div className="flex gap-4">
             <div className="flex flex-col w-1/2"></div>
             <div className="flex flex-col w-1/2">
-                <Detailcar />
+                <Detailcar
+                    vehicle={vehicle.name}
+                    transmission={vehicle.transmission}
+                    capacity={vehicle.capacity}
+                    engine={vehicle.engine}
+                    mileage={vehicle.mileage}
+                    fuel={vehicle.fuel}
+                    year={vehicle.year}
+                    colour={vehicle.colour}
+                    rate={vehicle.rate}
+                />
             </div>
         </div>
     )
