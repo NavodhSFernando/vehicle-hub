7b2f786b7eefcc419f1a49734bcfd777681fc641
Kanchana Herath <herathghmk.21@uom.lk>
2024-05-28 20:44:40 +0530

    Added Keyword search and some changes according to the backend.

--- src/components/front/VehicleFleet.jsx
+++ src/components/front/VehicleFleet.jsx
@@ -4,81 +4,81 @@ import BookingStrip2 from './BookingStrip/BookingStrip2'
 import SearchStrip from './BookingStrip/SearchStrip'
 import Aqua from '../../assets/vehicles/aqua.png'
 import FilterCard from './Filtercard'
-
-const data = [
-    {
-        key: '001',
-        name: 'Toyota aqua',
-        type: 'SUV',
-        imageSrc: Aqua,
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Nizan',
-        transmission: 'Manual',
-        capacity: '4 Persons',
-        price: '15000'
-    },
-    {
-        key: '002',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageSrc: Aqua,
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '6 Persons',
-        price: '15000'
-    },
-    {
-        key: '003',
-        name: 'Toyota prius',
-        type: 'SUV',
-        imageSrc: Aqua,
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '3 Persons',
-        price: '15000'
-    },
-    {
-        key: '004',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageSrc: Aqua,
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '6 Persons',
-        price: '15000'
-    },
-    {
-        key: '005',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageSrc: Aqua,
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '4 Persons',
-        price: '9000'
-    },
-    {
-        key: '006',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageSrc: Aqua,
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '4 Persons',
-        price: '6000'
-    }
-]
+import axios from 'axios'
+// const data = [
+//     {
+//         key: '001',
+//         name: 'Toyota aqua',
+//         type: 'SUV',
+//         imageSrc: Aqua,
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Nizan',
+//         transmission: 'Manual',
+//         capacity: '4 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '002',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageSrc: Aqua,
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '6 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '003',
+//         name: 'Toyota prius',
+//         type: 'SUV',
+//         imageSrc: Aqua,
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '3 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '004',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageSrc: Aqua,
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '6 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '005',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageSrc: Aqua,
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '4 Persons',
+//         price: '9000'
+//     },
+//     {
+//         key: '006',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageSrc: Aqua,
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '4 Persons',
+//         price: '6000'
+//     }
+// ]
 
 const VehicleFleet = () => {
     const [filters, setFilters] = useState({
@@ -88,53 +88,58 @@ const VehicleFleet = () => {
         maxPrice: 0
     })
 
-    const [keyword, setKeyword] = useState('')
-
     const [filteredData, setFilteredData] = useState([])
+
+    const [allVehicle, setAllVehicle] = useState([])
 
     const handleFilterChange = (newFilters) => {
         setFilters({ ...filters, ...newFilters })
     }
 
-    const handleSearch = (searchQuery) => {
-        setKeyword(searchQuery)
-        console.log(searchQuery)
-        const updatedFilteredData = data.filter((vehicle) =>
+    const handleSearch = async (searchQuery) => {
+        const updatedFilteredData = allVehicle.filter((vehicle) =>
             vehicle.name.toLowerCase().includes(searchQuery.toLowerCase())
         )
         setFilteredData(updatedFilteredData)
     }
 
+    const fetchAllVehicles = async () => {
+        try {
+            const response = await axios.get(`http://localhost:47367/api/Vehicle/alldata`)
+            const newVehicleData = response.data.map((item, index) => ({
+                key: `new-${index}`,
+                name: `${item.vehicleMake.name} ${item.vehicleModel.name}`,
+                imageSrc: Aqua,
+                imageAlt: `${item.vehicleMake.name} ${item.vehicleModel.name}`,
+                year: item.vehicleModel.year,
+                make: item.vehicleMake.name,
+                transmission: item.vehicle.transmission,
+                capacity: `${item.vehicleModel.seatingCapacity} Persons`,
+                price: item.vehicle.costPerDay.toString()
+            }))
+
+            setAllVehicle(newVehicleData)
+            setFilteredData(newVehicleData)
+        } catch (error) {
+            console.error('Error fetching vehicle data:', error)
+        }
+    }
+
     useEffect(() => {
-        const updatedFilteredData = data.filter((vehicle) => {
-            console.log('Vehicle:', vehicle)
-            console.log('Filters:', filters)
+        fetchAllVehicles()
+    }, [])
 
-            if (filters.vehicleType !== 'all' && vehicle.type !== filters.vehicleType) {
-                console.log('Filtered out due to vehicle type')
-                return false
-            }
-
-            if (filters.vehicleMake !== 'all' && vehicle.make !== filters.vehicleMake) {
-                console.log('Filtered out due to vehicle make')
-                return false
-            }
-
-            if (filters.vehicleCapacity !== 'all' && vehicle.capacity !== filters.vehicleCapacity) {
-                console.log('Filtered out due to vehicle capacity')
-                return false
-            }
-
-            if (parseInt(filters.maxPrice) > 0 && parseInt(vehicle.price) > parseInt(filters.maxPrice)) {
-                console.log('Filtered out due to price')
-                return false
-            }
-
+    useEffect(() => {
+        const updatedFilteredData = allVehicle.filter((vehicle) => {
+            if (filters.vehicleType !== 'all' && vehicle.type !== filters.vehicleType) return false
+            if (filters.vehicleMake !== 'all' && vehicle.make !== filters.vehicleMake) return false
+            if (filters.vehicleCapacity !== 'all' && vehicle.capacity !== filters.vehicleCapacity) return false
+            if (parseInt(filters.maxPrice) > 0 && parseInt(vehicle.price) > parseInt(filters.maxPrice)) return false
             return true
         })
 
         setFilteredData(updatedFilteredData)
-    }, [filters])
+    }, [filters, allVehicle])
 
     return (
         <div>
