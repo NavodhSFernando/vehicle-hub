d494b6e6b17c8609d334cd9a2fe2d95cd4559a75
Tharushi Samarasinghe <samarasinghesatlt.21@uom.lk>
2024-05-29 13:38:23 +0530

    fixed authentication issues, cookies, session storage

---
+++ src/axiosConfig.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export const getToken = () => {
+    return sessionStorage.getItem('jwtToken')
+}
+
+const apiclient = axios.create({
+    baseURL: 'http://localhost:5062/api',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+apiclient.interceptors.request.use(
+    (config) => {
+        const token = getToken()
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+export default apiclient
