becb0593f4889718bcad3ae0434701bdedb0a2f9
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-03 00:33:20 +0530

    updated vehicleInsurance

--- src/views/admin/insurance/DataTable.jsx
+++ src/views/admin/insurance/DataTable.jsx
@@ -32,14 +32,6 @@ export default function DataTable({ columns, data }) {
         <div>
             <div className="flex items-center py-4">
                 <Input
-                    placeholder="Filter ID..."
-                    value={table.getColumn('id')?.getFilterValue() ?? ''}
-                    onChange={(event) => table.getColumn('id')?.setFilterValue(event.target.value)}
-                    className="max-w-sm"
-                />
-            </div>
-            <div className="flex items-center py-4">
-                <Input
                     placeholder="Filter VehicleID..."
                     value={table.getColumn('vehicleId')?.getFilterValue() ?? ''}
                     onChange={(event) => table.getColumn('vehicleId')?.setFilterValue(event.target.value)}
