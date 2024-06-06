5c3c4f1a9310073e99da8cd6b9e76eaa1c5fcb6b
NavodhSFernando <fernandons.21@uom.lk>
2024-06-06 14:38:56 +0530

    Edit Reservation (Vehicle) FE done

--- src/components/admin/reservation/EditVehicleDialog.jsx
+++ src/components/admin/reservation/EditVehicleDialog.jsx
@@ -13,6 +13,76 @@ import {
 } from '../../ui/dialog'
 import { GrEdit } from 'react-icons/gr'
 import { ScrollArea } from '../../ui/scroll-area'
+import VehicleCard from './VehicleCard'
+
+const data = [
+    {
+        key: '001',
+        name: 'Aqua',
+        type: 'SUV',
+        imageAlt: 'Toyota Aqua',
+        year: '2017',
+        make: 'Nissan',
+        transmission: 'Manual',
+        capacity: '4 Persons',
+        price: '15000'
+    },
+    {
+        key: '002',
+        name: 'Toyota prius',
+        type: 'Sedan',
+        imageAlt: 'Toyota Aqua',
+        year: '2017',
+        make: 'Toyota',
+        transmission: 'Manual',
+        capacity: '6 Persons',
+        price: '15000'
+    },
+    {
+        key: '003',
+        name: 'Toyota prius',
+        type: 'SUV',
+        imageAlt: 'Toyota Aqua',
+        year: '2017',
+        make: 'Toyota',
+        transmission: 'Manual',
+        capacity: '3 Persons',
+        price: '15000'
+    },
+    {
+        key: '004',
+        name: 'Toyota prius',
+        type: 'Sedan',
+        imageAlt: 'Toyota Aqua',
+        year: '2017',
+        make: 'Toyota',
+        transmission: 'Manual',
+        capacity: '6 Persons',
+        price: '15000'
+    },
+    {
+        key: '005',
+        name: 'Toyota prius',
+        type: 'Sedan',
+        imageAlt: 'Toyota Aqua',
+        year: '2017',
+        make: 'Toyota',
+        transmission: 'Manual',
+        capacity: '4 Persons',
+        price: '9000'
+    },
+    {
+        key: '006',
+        name: 'Toyota prius',
+        type: 'Sedan',
+        imageAlt: 'Toyota Aqua',
+        year: '2017',
+        make: 'Toyota',
+        transmission: 'Manual',
+        capacity: '4 Persons',
+        price: '6000'
+    }
+]
 
 export function EditVehicleDialog() {
     const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
@@ -24,25 +94,26 @@ export function EditVehicleDialog() {
                     <GrEdit fontSize={24} className="mr-1" />
                 </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-md">
+            <DialogContent className="sm:max-w-xl">
                 <DialogHeader>
                     <DialogTitle>Available Vehicles</DialogTitle>
                     <DialogDescription>
                         List of available vehicles in accordance to reservation details.
                     </DialogDescription>
                 </DialogHeader>
-                <ScrollArea className="h-72 w-48 rounded-md border">
-                    <div className="p-4">
-                        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
-                        {tags.map((tag) => (
-                            <>
-                                <div key={tag} className="text-sm">
-                                    {tag}
-                                </div>
-                                {/* <Separator className="my-2" /> */}
-                            </>
-                        ))}
-                    </div>
+                <ScrollArea className="h-72 w-full border-none">
+                    {data.map((tag) => (
+                        <VehicleCard
+                            id={tag.id}
+                            name={tag.name}
+                            make={tag.make}
+                            type={tag.type}
+                            year={tag.year}
+                            transmission={tag.transmission}
+                            capacity={tag.capacity}
+                            price={tag.price}
+                        />
+                    ))}
                 </ScrollArea>
                 <DialogFooter className="sm:justify-start">
                     <DialogClose asChild>
--- src/components/admin/reservation/VehicleCard.jsx
+++ src/components/admin/reservation/VehicleCard.jsx
@@ -7,6 +7,7 @@ export default function VehicleCard({
     key,
     name,
     make,
+
     type,
     year,
     transmission,
@@ -16,8 +17,8 @@ export default function VehicleCard({
     price
 }) {
     return (
-        <div className="flex w-full p-5 shadow-xl rounded-xl bg-white">
-            <img className="w-1/3 booknowimage scale-x-[-1] rounded-xl" src={imageSrc} alt={imageAlt} />
+        <div className="flex w-full p-5 border border-slate-100 mb-2 rounded-xl bg-white">
+            <img className="w-1/4 booknowimage scale-x-[-1] rounded-xl" src={imageSrc} alt={imageAlt} />
             <div className="flex flex-col w-2/3 pl-5">
                 <div className="flex justify-between items-start pb-4">
                     <div className="flex flex-col">
@@ -27,7 +28,7 @@ export default function VehicleCard({
                         <p className="text-base opacity-50 font-semibold">{type}</p>
                     </div>
                 </div>
-                <div className="flex justify-between pb-4">
+                <div className="flex gap-10 pb-4">
                     <span className="flex gap-1 items-center">
                         <IoCalendarClear fontSize={20} style={{ color: '#90A3BF' }} />
                         <p className="text-sm opacity-50">{year}</p>
@@ -47,7 +48,6 @@ export default function VehicleCard({
                             {price}
                             <span className="text-sm opacity-50"> / Day</span>
                         </h1>
-                        <p className="text-sm opacity-50 font-semibold">100Km/day</p>
                     </span>
                     <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700">
                         Select
