6fed72c62a300cef44ef540263dd167e0a96211a
Tharushi Samarasinghe <samarasinghesatlt.21@uom.lk>
2024-06-08 15:32:18 +0530

    admin login backend connected

--- src/views/admin/Adminlogin.jsx
+++ src/views/admin/Adminlogin.jsx
@@ -1,47 +1,118 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import admin from '../../../src/assets/logos/admin.png'
+import { Button } from '../../components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../../components/ui/form'
+import { Input } from '../../components/ui/input'
+
+const formSchema = z.object({
+    email: z.string().email('Invalid email format').min(1, 'Email is required'),
+    password: z.string().min(8, 'Password must be at least 8 characters long')
+})
 
 function Adminlogin() {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    })
+    const navigate = useNavigate()
+
+    const onSubmit = async (data) => {
+        try {
+            const result = await axios.post('http://localhost:5062/api/EmployeeAuth/login', data)
+            console.log(result.data)
+            const token = result.data
+            sessionStorage.setItem('jwtToken', token)
+            // Assuming that the backend returns the employee ID within the token payload
+            const employeeId = result.data.token.id
+            Cookies.set('employeeId', employeeId, { expires: 30 })
+            console.log(`Employee ID saved in cookies: ${Cookies.get('employeeId')}`)
+            navigate('/admin/dashboard')
+        } catch (error) {
+            console.error('Login failed', error.response ? error.response.data : error.message)
+        }
+    }
+
     return (
-        <div>
-            <div className="relative min-h-screen flex">
-                <div className="w-1/2 h-full overflow-hidden">
-                    <div className="bg-blue-800 h-full">
-                        <img />
-                    </div>
+        <div className="relative min-h-screen flex">
+            <div className="w-1/2 h-full overflow-hidden">
+                <div className="bg-blue-800 h-full flex items-center justify-center">
+                    <img src={admin} alt="background" className="object-cover" />
                 </div>
-                <div className="w-1/2 flex justify-center items-center bg-gray-100">
-                    <div className="flex flex-col w-full max-w-md px-8 py-10 bg-white shadow-md rounded-lg">
-                        <form action="post" className="w-full space-y-6">
-                            <h1 className="font-bold text-gray-800 text-xl">Admin Login</h1>
-                            <div className="flex flex-col space-y-1">
-                                <label htmlFor="username" className="text-gray-800 font-semibold">
-                                    Username
-                                </label>
-                                <input className="p-2 bg-white border border-gray-300 rounded-lg text-s text-gray-500" />
-                            </div>
-                            <div className="flex flex-col space-y-1">
-                                <label htmlFor="password" className="text-gray-800 font-semibold">
-                                    Password
-                                </label>
-                                <input className="p-2 bg-white border border-gray-300 rounded-lg text-s text-gray-500" />
-                            </div>
+            </div>
+            <div className="w-1/2 flex justify-center items-center bg-gray-100">
+                <div className="flex flex-col w-1/2 max-w-md px-6 py-6 bg-white shadow-md rounded-lg">
+                    <h1 className="font-bold text-gray-600 text-xl text-center">Admin Login</h1>
+                    <p className="text-sm font-inter text-gray-600 pb-4 text-center">Please enter your credentials.</p>
+                    <Form {...control}>
+                        <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-6">
+                            <FormField
+                                control={control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel htmlFor="email" className="text-gray-800 font-semibold">
+                                            Username
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                id="email"
+                                                {...field}
+                                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-indigo-600"
+                                            />
+                                        </FormControl>
+                                        <FormMessage>{errors.username?.message}</FormMessage>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel htmlFor="password" className="text-gray-800 font-semibold">
+                                            Password
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                id="password"
+                                                type="password"
+                                                {...field}
+                                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-indigo-600"
+                                            />
+                                        </FormControl>
+                                        <FormMessage>{errors.password?.message}</FormMessage>
+                                    </FormItem>
+                                )}
+                            />
                             <div className="flex items-center">
-                                <input type="checkbox" id="remember" className="h-4 w-4" />
-                                <label htmlFor="remember-me" className="ml-2 text-gray-800">
+                                <FormControl>
+                                    <input type="checkbox" id="remember" className="h-4 w-4" />
+                                </FormControl>
+                                <FormLabel htmlFor="remember" className="ml-2 text-gray-800">
                                     Remember me
-                                </label>
-                                <br></br>
+                                </FormLabel>
                             </div>
-                            <div>
-                                <button
-                                    type="submit"
-                                    className="w-full py-3 text-sm text-center tracking-wide bg-indigo-800 text-yellow-200 rounded-md font-semibold hover:bg-indigo-900 focus:outline-none focus:ring focus:ring-indigo-500"
-                                >
-                                    Log in
-                                </button>
-                            </div>
+                            <Button
+                                type="submit"
+                                className="w-full py-3 text-sm text-center tracking-wide bg-indigo-800 text-yellow-200 rounded-md font-semibold hover:bg-indigo-900 focus:outline-none focus:ring focus:ring-indigo-500"
+                            >
+                                Log in
+                            </Button>
                         </form>
-                    </div>
+                    </Form>
                 </div>
             </div>
         </div>
