7283ee03d32835b28f7f04d95899a5bea2cec850
NavodhSFernando <fernandons.21@uom.lk>
2024-06-15 17:30:28 +0530

    vehicle change in reservation

--- src/components/admin/reservation/EditVehicleDialog.jsx
+++ src/components/admin/reservation/EditVehicleDialog.jsx
@@ -1,5 +1,3 @@
-import { Copy } from 'lucide-react'
-
 import { Button } from '../../ui/button'
 import {
     Dialog,
@@ -14,78 +12,94 @@ import {
 import { GrEdit } from 'react-icons/gr'
 import { ScrollArea } from '../../ui/scroll-area'
 import VehicleCard from './VehicleCard'
+import { useState, useEffect } from 'react'
+import { get } from 'react-hook-form'
+import axios from 'axios'
 
-const data = [
-    {
-        key: '001',
-        name: 'Aqua',
-        type: 'SUV',
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Nissan',
-        transmission: 'Manual',
-        capacity: '4 Persons',
-        price: '15000'
-    },
-    {
-        key: '002',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '6 Persons',
-        price: '15000'
-    },
-    {
-        key: '003',
-        name: 'Toyota prius',
-        type: 'SUV',
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '3 Persons',
-        price: '15000'
-    },
-    {
-        key: '004',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '6 Persons',
-        price: '15000'
-    },
-    {
-        key: '005',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '4 Persons',
-        price: '9000'
-    },
-    {
-        key: '006',
-        name: 'Toyota prius',
-        type: 'Sedan',
-        imageAlt: 'Toyota Aqua',
-        year: '2017',
-        make: 'Toyota',
-        transmission: 'Manual',
-        capacity: '4 Persons',
-        price: '6000'
-    }
-]
+// const data = [
+//     {
+//         key: '001',
+//         name: 'Aqua',
+//         type: 'SUV',
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Nissan',
+//         transmission: 'Manual',
+//         capacity: '4 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '002',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '6 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '003',
+//         name: 'Toyota prius',
+//         type: 'SUV',
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '3 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '004',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '6 Persons',
+//         price: '15000'
+//     },
+//     {
+//         key: '005',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '4 Persons',
+//         price: '9000'
+//     },
+//     {
+//         key: '006',
+//         name: 'Toyota prius',
+//         type: 'Sedan',
+//         imageAlt: 'Toyota Aqua',
+//         year: '2017',
+//         make: 'Toyota',
+//         transmission: 'Manual',
+//         capacity: '4 Persons',
+//         price: '6000'
+//     }
+// ]
 
-export function EditVehicleDialog() {
-    const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
+export function EditVehicleDialog({ customerReservationId, refetchReservation }) {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const getAvailableVehicles = async () => {
+            const url = `http://localhost:5062/api/AdminReservation/Available-Vehicles/${customerReservationId}`
+            try {
+                const response = await axios.get(url)
+                console.log(response.data)
+                setData(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getAvailableVehicles()
+    }, [customerReservationId])
 
     return (
         <Dialog>
@@ -104,24 +118,21 @@ export function EditVehicleDialog() {
                 <ScrollArea className="h-72 w-full border-none">
                     {data.map((tag) => (
                         <VehicleCard
+                            key={tag.key}
                             id={tag.id}
                             name={tag.name}
                             make={tag.make}
                             type={tag.type}
                             year={tag.year}
                             transmission={tag.transmission}
-                            capacity={tag.capacity}
-                            price={tag.price}
+                            capacity={tag.seatingCapacity}
+                            price={tag.costPerDay}
+                            imageSrc={tag.thumbnail}
+                            customerReservationId={customerReservationId}
+                            refetchReservation={refetchReservation}
                         />
                     ))}
                 </ScrollArea>
-                <DialogFooter className="sm:justify-start">
-                    <DialogClose asChild>
-                        <Button type="button" variant="secondary">
-                            Close
-                        </Button>
-                    </DialogClose>
-                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
--- src/components/admin/reservation/VehicleCard.jsx
+++ src/components/admin/reservation/VehicleCard.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import { HiUsers } from 'react-icons/hi'
 import { IoCalendarClear } from 'react-icons/io5'
@@ -5,20 +6,35 @@ import { RiSteering2Fill } from 'react-icons/ri'
 
 export default function VehicleCard({
     key,
+    id,
     name,
     make,
-
     type,
     year,
     transmission,
     capacity,
     imageSrc,
-    imageAlt,
-    price
+    price,
+    customerReservationId,
+    refetchReservation
 }) {
+    const baseUrl = 'https://vehiclehubimages.blob.core.windows.net/thumbnails/'
+
+    const handleSelect = async () => {
+        const url = `http://localhost:5062/api/AdminReservation/Change-Vehicle/${customerReservationId}?vid=${id}`
+        try {
+            const response = await axios.post(url)
+            console.log(response.data)
+            console.log('Vehicle selected')
+            refetchReservation()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="flex w-full p-5 border border-slate-100 mb-2 rounded-xl bg-white">
-            <img className="w-1/4 booknowimage scale-x-[-1] rounded-xl" src={imageSrc} alt={imageAlt} />
+            <img className="w-1/3 rounded-xl" src={imageSrc ? `${baseUrl}${imageSrc}` : null} />
             <div className="flex flex-col w-2/3 pl-5">
                 <div className="flex justify-between items-start pb-4">
                     <div className="flex flex-col">
@@ -49,7 +65,10 @@ export default function VehicleCard({
                             <span className="text-sm opacity-50"> / Day</span>
                         </h1>
                     </span>
-                    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700">
+                    <button
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700"
+                        onClick={() => handleSelect()}
+                    >
                         Select
                     </button>
                 </div>
