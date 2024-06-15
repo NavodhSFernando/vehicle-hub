a99bf331c510590088641e2d973aae5d239be2b6
Hirushi Aramandeniya <aramandeniyaaghn.21@uom.lk>
2024-06-15 17:35:39 +0530

    DataTables modified

--- src/views/admin/reservation/DataTable.jsx
+++ src/views/admin/reservation/DataTable.jsx
@@ -5,7 +5,8 @@ import {
     getCoreRowModel,
     useReactTable,
     getFilteredRowModel,
-    getSortedRowModel
+    getSortedRowModel,
+    getPaginationRowModel
 } from '@tanstack/react-table'
 import { Input } from '../../../components/ui/input'
 import { Label } from '../../../components/ui/label'
@@ -16,6 +17,7 @@ import { Popover, PopoverContent, PopoverTrigger } from '../../../components/ui/
 import { Calendar as CalendarIcon } from 'lucide-react'
 import { format } from 'date-fns'
 import cn from 'classnames'
+import { DataTablePagination } from '../../../components/ui/DataTablePagination'
 
 export default function DataTable({ columns, data }) {
     const [columnFilters, setColumnFilters] = React.useState([])
@@ -28,13 +30,15 @@ export default function DataTable({ columns, data }) {
         columns,
         getCoreRowModel: getCoreRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
         onSortingChange: setSorting,
         onColumnFiltersChange: setColumnFilters,
         getSortedRowModel: getSortedRowModel(),
         state: {
             sorting,
             columnFilters
-        }
+        },
+        initialState: { pagination: { pageSize: 5 } }
     })
 
     // Update the date filter when the date state changes
@@ -148,7 +152,7 @@ export default function DataTable({ columns, data }) {
                             <TableRow className="bg-slate-200" key={headerGroup.id}>
                                 {headerGroup.headers.map((header) => {
                                     return (
-                                        <TableHead className="py-1 px-5" key={header.id}>
+                                        <TableHead key={header.id}>
                                             {header.isPlaceholder
                                                 ? null
                                                 : flexRender(header.column.columnDef.header, header.getContext())}
@@ -163,7 +167,7 @@ export default function DataTable({ columns, data }) {
                             table.getRowModel().rows.map((row) => (
                                 <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell className="py-5 px-5" key={cell.id}>
+                                        <TableCell key={cell.id}>
                                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     ))}
--- src/views/admin/vehiclelog/DataTable.jsx
+++ src/views/admin/vehiclelog/DataTable.jsx
@@ -6,14 +6,16 @@ import {
     getCoreRowModel,
     useReactTable,
     getFilteredRowModel,
-    getSortedRowModel
+    getSortedRowModel,
+    getPaginationRowModel
 } from '@tanstack/react-table'
 import { Input } from '../../../components/ui/input'
+import { DataTablePagination } from '../../../components/ui/DataTablePagination'
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../../components/ui/table'
 
 export default function DataTable({ columns, data }) {
-    //const [columnFilters, setColumnFilters] = React.useState([])
+    const [columnFilters, setColumnFilters] = React.useState([])
     const [sorting, setSorting] = React.useState([])
 
     const table = useReactTable({
@@ -21,11 +23,15 @@ export default function DataTable({ columns, data }) {
         columns,
         getCoreRowModel: getCoreRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
         onSortingChange: setSorting,
+        onColumnFiltersChange: setColumnFilters,
         getSortedRowModel: getSortedRowModel(),
         state: {
-            sorting
-        }
+            sorting,
+            columnFilters
+        },
+        initialState: { pagination: { pageSize: 5 } }
     })
 
     return (
@@ -38,7 +44,7 @@ export default function DataTable({ columns, data }) {
                             <TableRow className="bg-slate-200" key={headerGroup.id}>
                                 {headerGroup.headers.map((header) => {
                                     return (
-                                        <TableHead className="py-1 px-5" key={header.id}>
+                                        <TableHead key={header.id}>
                                             {header.isPlaceholder
                                                 ? null
                                                 : flexRender(header.column.columnDef.header, header.getContext())}
@@ -53,7 +59,7 @@ export default function DataTable({ columns, data }) {
                             table.getRowModel().rows.map((row) => (
                                 <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell className="py-5 px-5" key={cell.id}>
+                                        <TableCell key={cell.id}>
                                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     ))}
