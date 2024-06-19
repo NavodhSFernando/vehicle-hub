7dd3fc3ac1d227b7a511a3b34a4a71b281030a31
NavodhSFernando <fernandons.21@uom.lk>
2024-06-20 01:22:51 +0530

    layout change

--- src/components/front/Layout.jsx
+++ src/components/front/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
     const isHomePage = pathname === '/'
 
     return (
-        <div className="bg-slate-100">
+        <div className="bg-slate-200">
             <NavBar />
             <div className={`${!isHomePage ? 'max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-28 lg:min-h-[700px]' : ''}`}>
                 <Outlet />
