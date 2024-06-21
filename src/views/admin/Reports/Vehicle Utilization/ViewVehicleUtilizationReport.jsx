244e375240bf81ecf079a101474c0582d42d791b
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-06-21 20:59:23 +0530

    uasge chart - Vehicle utilization report

--- src/views/admin/Reports/Vehicle Utilization/ViewVehicleUtilizationReport.jsx
+++ src/views/admin/Reports/Vehicle Utilization/ViewVehicleUtilizationReport.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import VehicleUtilizationTable from './VehicleUtilizationTable' 
+import UsageDurationChart from './UsageDurationChart';
+
 
 const ViewVehicleUtilizationReport = () => {
     return (
-        
+        <div>
             <VehicleUtilizationTable />
-        
+            <UsageDurationChart />
+        </div>
     );
 }
 
