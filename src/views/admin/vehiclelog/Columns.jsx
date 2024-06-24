5dd0db4f4a65b794e29db1bfd96525ea7e9d3b67
NavodhSFernando <fernandons.21@uom.lk>
2024-06-24 13:12:30 +0530

    reservation based dialogs and alerts added

--- src/views/admin/vehiclelog/Columns.jsx
+++ src/views/admin/vehiclelog/Columns.jsx
@@ -1,18 +1,21 @@
 import { Button } from '../../../components/ui/button'
-import { GrEdit, GrTrash } from 'react-icons/gr'
+import { GrEdit } from 'react-icons/gr'
 import { useNavigate } from 'react-router-dom'
+import { AlertDialogDemo } from '../../../components/ui/alertDialog'
 
 // Define a component to encapsulate the action buttons
 const ActionButtons = ({ vehicleLogId }) => {
     const navigate = useNavigate()
     return (
         <div className="flex items-center justify-end gap-2">
-            <Button variant="ghost" className="p-0" onClick={() => navigate(`/admin/vehiclelog/edit/${vehicleLogId}`)}>
-                <GrEdit fontSize={24} className="mr-1" />
-            </Button>
-            <Button variant="ghost" className="p-0">
-                <GrTrash fontSize={24} className="mr-1" />
-            </Button>
+            <AlertDialogDemo
+                triggerText={<GrEdit fontSize={20} className="mr-1" />}
+                alertTitle="Edit Vehicle Log"
+                alertDescription="Are you sure you want to end this reservation?"
+                handleConfirm={() => navigate(`/admin/vehiclelog/edit/${vehicleLogId}`)}
+                buttonClass="p-0"
+                variant="ghost"
+            />
         </div>
     )
 }
