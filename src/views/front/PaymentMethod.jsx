84d5b2f2bfd035a3e35d4ed7b887bfa50d607d43
Tharushi Samarasinghe <samarasinghesatlt.21@uom.lk>
2024-07-01 12:56:12 +0530

    added toast messages

--- src/views/front/PaymentMethod.jsx
+++ src/views/front/PaymentMethod.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import { FaCcVisa, FaCcMastercard } from 'react-icons/fa';
-import axios from "axios";
+import React, { useState } from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
+import { FaCcVisa, FaCcMastercard } from 'react-icons/fa'
+import axios from 'axios'
+import { useToast } from '../../components/ui/use-toast'
 
 // Load Stripe
-const stripePromise = loadStripe("pk_test_51PKzzr2NtGTfzr39JL9elmrssZNbhuSPuz0NBmql7gZntZqk8O1pRKaNqkjd6mPskyRk1Fgavgp8ATRM7BZWEcdK00qJ7Rx4UR");
+const stripePromise = loadStripe(
+    'pk_test_51PKzzr2NtGTfzr39JL9elmrssZNbhuSPuz0NBmql7gZntZqk8O1pRKaNqkjd6mPskyRk1Fgavgp8ATRM7BZWEcdK00qJ7Rx4UR'
+)
 
 // Combined Payment Method component
 const PaymentMethod = ({ invoiceId, amount, invoiceType }) => {
     return (
         <Elements stripe={stripePromise}>
-            <PaymentForm 
-                invoiceId={invoiceId} 
-                amount={amount} 
-                invoiceType={invoiceType} 
-            />
+            <PaymentForm invoiceId={invoiceId} amount={amount} invoiceType={invoiceType} />
         </Elements>
-    );
-};
+    )
+}
 
 // Payment Form component
 const PaymentForm = ({ invoiceId, amount, invoiceType }) => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const [cardholderName, setCardholderName] = useState("");
-    const [message, setMessage] = useState("");
-    const [isSuccess, setIsSuccess] = useState(false);
+    const stripe = useStripe()
+    const elements = useElements()
+    const [cardholderName, setCardholderName] = useState('')
+    const [message, setMessage] = useState('')
+    const [isSuccess, setIsSuccess] = useState(false)
+    const { toast } = useToast()
 
     const handleInputChange = (e) => {
-        setCardholderName(e.target.value);
-    };
+        setCardholderName(e.target.value)
+    }
 
     const handlePayment = async () => {
         try {
-            const response = await axios.post("http://localhost:5062/api/Payments/create-payment-intent", {
+            const response = await axios.post('http://localhost:5062/api/Payments/create-payment-intent', {
                 amount: amount * 100, // Assuming amount is in dollars and needs to be in cents for Stripe
-                currency: "lkr",
+                currency: 'lkr',
                 invoiceType // Send invoiceType if needed in backend
-            });
+            })
 
-            const clientSecret = response.data.clientSecret;
+            const clientSecret = response.data.clientSecret
 
             const { paymentIntent, error } = await stripe.confirmCardPayment(clientSecret, {
                 payment_method: {
@@ -49,59 +49,65 @@ const PaymentForm = ({ invoiceId, amount, invoiceType }) => {
                         name: cardholderName
                     }
                 }
-            });
+            })
 
             if (error) {
-                console.error("Payment failed:", error);
-                setMessage("Your payment was not successful, please try again.");
-                setIsSuccess(false);
-            } else if (paymentIntent.status === "succeeded") {
-                setMessage("Payment succeeded!");
-                setIsSuccess(true);
+                console.error('Payment failed:', error)
+                toast({
+                    variant: 'destructive_border',
+                    description: 'Payment was not successful!'
+                })
+                setIsSuccess(false)
+            } else if (paymentIntent.status === 'succeeded') {
+                toast({
+                    variant: 'success',
+                    description: 'Payment was successful!'
+                })
+                setIsSuccess(true)
 
                 // Prepare payment data excluding id
                 const paymentData = {
                     paymentStatus: invoiceType,
-                    paymentMethod: "Card",
+                    paymentMethod: 'Card',
                     paymentDate: new Date().toISOString(),
                     paymentTime: new Date().toISOString(),
                     invoiceId: invoiceId
-                };
+                }
 
                 // Send POST request to save payment data
                 await axios.post(`http://localhost:5062/api/Payment`, paymentData, {
                     headers: {
-                        'accept': 'text/plain',
+                        accept: 'text/plain',
                         'Content-Type': 'application/json'
                     }
-                });
+                })
 
                 // Determine reservation status based on invoice type
-                const newStatus = invoiceType === "Deposit" ? "Confirmed" : "Completed";
+                const newStatus = invoiceType === 'Deposit' ? 'Confirmed' : 'Completed'
 
                 // Prepare data for updating reservation status
                 const reservationStatusData = {
                     invoiceId: invoiceId,
                     newStatus: newStatus
-                };
+                }
 
                 // Send PUT request to update reservation status
                 await axios.put(`http://localhost:5062/api/Payment/UpdateReservationStatus`, reservationStatusData, {
                     headers: {
-                        'accept': '*/*',
+                        accept: '*/*',
                         'Content-Type': 'application/json'
                     }
-                });
+                })
             } else {
-                setMessage("Your payment is processing.");
-                setIsSuccess(false);
+                setMessage('Your payment is processing.')
+                setIsSuccess(false)
             }
         } catch (error) {
-            console.error("Error handling payment:", error);
-            setMessage("Something went wrong.");
-            setIsSuccess(false);
+            console.error('Error handling payment:', error)
+            setMessage('Something went wrong.')
+            setIsSuccess(false)
         }
-    };
+    }
 
     return (
         <div className="flex flex-col w-full bg-white rounded-xl shadow-lg mb-1 p-10 relative">
@@ -130,14 +136,14 @@ const PaymentForm = ({ invoiceId, amount, invoiceType }) => {
                         options={{
                             style: {
                                 base: {
-                                    fontSize: "16px",
-                                    color: "#424770",
-                                    "::placeholder": {
-                                        color: "#aab7c4"
+                                    fontSize: '16px',
+                                    color: '#424770',
+                                    '::placeholder': {
+                                        color: '#aab7c4'
                                     }
                                 },
                                 invalid: {
-                                    color: "#9e2146"
+                                    color: '#9e2146'
                                 }
                             }
                         }}
@@ -152,11 +158,9 @@ const PaymentForm = ({ invoiceId, amount, invoiceType }) => {
                 Pay
             </button>
 
-            {message && (
-                <p className={`mt-4 ${isSuccess ? 'text-green-600' : 'text-red-600'}`}>{message}</p>
-            )}
+            {message && <p className={`mt-4 ${isSuccess ? 'text-green-600' : 'text-red-600'}`}>{message}</p>}
         </div>
-    );
-};
+    )
+}
 
-export default PaymentMethod;
+export default PaymentMethod
